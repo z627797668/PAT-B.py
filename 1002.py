20a3f883f7722e540b75eced2439ba9b7b82ca84
z627797668 <627797668@qq.com>
2018-06-04 17:18:02 +0800

    Create 1002.py

---
+++ 1002.py
@@ -0,0 +1,21 @@
+str=input()
+sum=0
+i=0
+num=[]
+ansstr=["ling","yi","er","san","si","wu","liu","qi","ba","jiu"]
+for ch in str:
+    sum+=int(ch)
+if sum==0:
+    print("ling")
+else:
+    while sum>0:
+        num.append(int(sum%10))
+        i+=1
+        sum=sum//10
+    i-=1
+    while i>=0: 
+        if i!=0:
+            print(ansstr[num[i]],end=' ')
+        else:
+            print(ansstr[num[i]])
+        i-=1
